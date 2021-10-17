5d2f44031e5a08b857c2ac789d2083a7990c4f77
Jimmy <changchiyin@gmail.com>
2021-10-17 13:26:02 +0800

    first version

---
+++ HW1/HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  HW1App.swift
+//  HW1
+//
+//  Created by ljy on 2021/10/17.
+//
+
+import SwiftUI
+
+@main
+struct HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
